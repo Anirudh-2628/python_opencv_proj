20e4f0743092965535efc5670e60fc3b2da8ceb5
Anirudh-2628 <63852463+Anirudh-2628@users.noreply.github.com>
2021-10-27 21:26:11 +0530

    Create documentscn.py

---
+++ documentscn.py
@@ -0,0 +1,49 @@
+import cv2
+import numpy as np
+
+
+circles = np.zeros((4,2), np.int0)
+
+counter = 0
+
+
+
+def mousePoints(events,x,y,flags,params):
+    global counter
+    if events==cv2.EVENT_LBUTTONDOWN:
+        circles[counter] = x,y
+        counter = counter + 1
+        print(circles)
+
+
+img = cv2.imread("sample.png")
+
+
+while True:
+    if counter == 4:
+        width, height = 250,350
+
+        pts1 = np.float32([circles[0],circles[1],circles[2],circles[3]])
+        
+        pts2 = np.float32([[0, 0], [width, 0], [0, height], [width, height]])
+
+        matrix = cv2.getPerspectiveTransform(pts1, pts2)
+
+        output = cv2.warpPerspective(img, matrix, (width, height))
+
+        cv2.imshow("output image", output)
+
+    for x in range(0, 4):
+        cv2.circle(img, (int(circles[x][0]), int(circles[x][1])),3, (0, 0, 255), cv2.FILLED)
+    cv2.imshow("output", img)
+    cv2.setMouseCallback("output", mousePoints)
+    cv2.waitKey(1)
+
+
+
+
+
+
+
+
+    
